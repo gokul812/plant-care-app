5e7507365736fe34a18cac5c7a4e2c41cf3d6456
unknown <gokulnair12102018@gmail.com>
2026-03-26 11:54:49 +0530

    fixed backend code

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
