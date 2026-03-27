b37ae4668e48d946aab4927e83d10a74437e5d8d
unknown <gokulnair12102018@gmail.com>
2026-03-27 14:09:47 +0530

    changes done

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,9 @@
-import { v2 as cloudinary } from "cloudinary";
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-export default cloudinary;
+module.exports = cloudinary;
